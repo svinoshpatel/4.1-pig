dbe30af829086736f1b2db1e218fdc0749b0602b
svinoshpatel <132487936+svinoshpatel@users.noreply.github.com>
2025-03-26 07:04:15 -0700

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,39 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = #1044#1077#1084#1086#1085#1089#1090#1088#1072#1094#1110#1103' '#1089#1074#1080#1085#1077#1081
+  ClientHeight = 161
+  ClientWidth = 384
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 140
+    Top = 36
+    Width = 89
+    Height = 15
+    Caption = #1053#1072#1090#1080#1089#1085#1080' '#1082#1085#1086#1087#1082#1091
+  end
+  object ButtonPetPig: TButton
+    Left = 50
+    Top = 70
+    Width = 120
+    Height = 30
+    Caption = #1044#1086#1084#1072#1096#1085#1103' '#1089#1074#1080#1085#1082#1072
+    TabOrder = 0
+    OnClick = ButtonPetPigClick
+  end
+  object ButtonFarmPig: TButton
+    Left = 200
+    Top = 70
+    Width = 120
+    Height = 30
+    Caption = #1060#1077#1088#1084#1077#1088#1089#1100#1082#1072' '#1089#1074#1080#1085#1082#1072
+    TabOrder = 1
+    OnClick = ButtonFarmPigClick
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,80 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.Forms, Vcl.StdCtrls;
+
+type
+  // Абстрактний клас TPig
+  TPig = class
+    function Speak: String; virtual; abstract;
+  end;
+
+  // Клас-нащадок TPetPig (домашня свинка)
+  TPetPig = class(TPig)
+    //function Speak: String; //override;
+  end;
+
+  // Клас-нащадок TFarmPig (фермерська свиня)
+  TFarmPig = class(TPig)
+    function Speak: String; override;
+  end;
+
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    ButtonPetPig: TButton;
+    ButtonFarmPig: TButton;
+    procedure ButtonPetPigClick(Sender: TObject);
+    procedure ButtonFarmPigClick(Sender: TObject);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+// Реалізація методу Speak для домашньої свинки
+//function TPetPig.Speak: String;
+//begin
+  //Result := 'Хрю-хрю! Я домашня свинка!';
+//end;
+
+// Реалізація методу Speak для фермерської свині
+function TFarmPig.Speak: String;
+begin
+  Result := 'Гррр-хрю! Я велика фермерська свиня!';
+end;
+
+// Обробник натискання кнопки для домашньої свинки
+procedure TForm1.ButtonPetPigClick(Sender: TObject);
+var
+  Pig: TPig;
+  PetPig: TPetPig;
+begin
+  Pig := TPig.Create;
+  PetPig := TPetPig.Create;
+  try
+    Label1.Caption := PetPig.Speak;
+  finally
+    PetPig.Free;
+  end;
+end;
+
+// Обробник натискання кнопки для фермерської свині
+procedure TForm1.ButtonFarmPigClick(Sender: TObject);
+var
+  FarmPig: TFarmPig;
+begin
+  FarmPig := TFarmPig.Create;
+  try
+    Label1.Caption := FarmPig.Speak;
+  finally
+    FarmPig.Free;
+  end;
+end;
+
+end.
+
